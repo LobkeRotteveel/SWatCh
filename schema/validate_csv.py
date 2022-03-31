771ee6c8f6e8f94d09ee45dd21c27c50b81336bd
Franz Heubach <franz.heubach@gmail.com>
2022-03-31 16:19:33 -0300

    Add progress bar for validation
    Progress bar works correctly event when starting at a later row.

--- schema/validate_csv.py
+++ schema/validate_csv.py
@@ -6,6 +6,7 @@ import pandas as pd
 import json
 from pprint import pprint
 import argparse
+from tqdm import tqdm
 
 
 def init_cli():
@@ -50,6 +51,11 @@ def get_column_dtypes_from_schema(schema):
     return column_dtypes
 
 
+def lines_in_file(file_path):
+    with open(file_path) as f:
+        return sum(1 for _ in f)
+
+
 def main():
     cli = init_cli()
     args = cli.parse_args()
@@ -58,28 +64,35 @@ def main():
         schema = json.load(file)
 
     dtypes = get_column_dtypes_from_schema(schema)
+    csv_rows = lines_in_file(args.csv_path)
     df = pd.read_csv(
-        args.csv_path, dtype=dtypes, skiprows=lambda x: x != 0 and x < args.start_row
+        args.csv_path,
+        dtype=dtypes,
+        skiprows=lambda x: x != 0 and x < args.start_row,
     )
     json_string = df.to_json(orient="records")
     data = json.loads(json_string)
 
     fails = 0
-    for i, record in enumerate(data):
-        if fails >= args.max_fails:
-            break
-        try:
-            validate(instance=record, schema=schema)
-        except jsonschema.exceptions.ValidationError as e:
-            print(f"row: {i}")
-            print(f"{e.validator} validator failed because: {e.message}")
-            print(f"offending json element:")
-            pprint(e.instance)
-            print(f"json path: {e.json_path}")
-            print(f"applicable schema:")
-            print(e.schema)
-            print()
-            fails += 1
+    with tqdm(total=csv_rows - 1) as pbar:
+        pbar.n = args.start_row - 1
+        pbar.refresh()
+        for i, record in enumerate(data):
+            if fails >= args.max_fails:
+                break
+            try:
+                validate(instance=record, schema=schema)
+            except jsonschema.exceptions.ValidationError as e:
+                print(f"row: {i}")
+                print(f"{e.validator} validator failed because: {e.message}")
+                print(f"offending json element:")
+                pprint(e.instance)
+                print(f"json path: {e.json_path}")
+                print(f"applicable schema:")
+                print(e.schema)
+                print()
+                fails += 1
+            pbar.update(1)
 
 
 if __name__ == "__main__":
